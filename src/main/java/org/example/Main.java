c3d6d0f50b463701148cd1a4124703bce478b454
깃waimi3169 <waimi3169@naver.com>
2023-03-02 10:17:52 +0900

    3ë²단계 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -36,6 +36,7 @@ class App {
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
     }
