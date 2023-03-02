81456aad4b181eec56d86e0f49d7d97d770433f1
깃waimi3169 <waimi3169@naver.com>
2023-03-02 11:20:11 +0900

    5ë5단계 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,32 +17,5 @@ public class Main {
         sc.close();
     }
 }
-class App {
-    private final Scanner sc;
-    public App(Scanner sc) {
-        this.sc=sc;
-    }
-    public void run() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("== 명언 앱 ==");
 
-        long lastid = 1;
-
-        while (true) {
-            System.out.print("명령) ");
-            String a = sc.nextLine().trim();
-
-            if (a.equals("종료")) {
-                break;
-            } else if (a.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = sc.nextLine().trim();
-                System.out.print("작가 : ");
-                String author = sc.nextLine().trim();
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastid);
-                lastid++;
-            }
-        }
-    }
-}
 
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class WiseSaying {
+    private long id;
+    private String content;
+    private String author;
+
+    public WiseSaying(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
