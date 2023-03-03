4888bb1649cc04567ad5a07f26ec06b2391b64e7
깃waimi3169 <waimi3169@naver.com>
2023-03-03 10:04:28 +0900

    7단계 구현

--- src/main/java/org/example/ll/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/org/example/ll/wiseSaying/controller/WiseSayingController.java
@@ -50,6 +50,11 @@ public class WiseSayingController {
 
         WiseSaying wiseSaying = findById(id);
 
+        if (wiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+            return;
+        }
+
         wiseSayings.remove(wiseSaying);
 
         System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
